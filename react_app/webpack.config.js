e129dc8fe0abe698db402b92dc61881aa8c5dd54
Milind Mahajan <milinds.mahajan@tcs.com>
2016-08-09 21:13:47 +0530

    Add files via upload

---
+++ react_app/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+            entry: [
+              './source/app.jsx'
+            ],
+            output: {
+              path: __dirname,
+              filename: "bundle.js"
+}, module: {
+              loaders: [{
+                test: /\.jsx?$/,
+                loader: 'babel'
+}] }
+};
